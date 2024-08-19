60daaec099b082c113078532033b24d547761c4f
nshib <nshib54321@gmail.com>
2024-08-19 15:13:11 +0300

    Create join hotels method in bookings service, move app path insert in bookings

--- fastapi_course_app/app/bookings/router.py
+++ fastapi_course_app/app/bookings/router.py
@@ -1,17 +1,13 @@
 from datetime import date
 from fastapi import APIRouter, Depends
-import sys
-from pathlib import Path
-
-
-app_path = Path(__file__).parent.parent.parent
-sys.path.insert(0, str(app_path))
+from pydantic import TypeAdapter
 
 from app.bookings.schemas import BookingsResponseSchema
 from app.bookings.service import BookingsService
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 from app.exceptions import RoomCannotBeBookedException
+from fastapi_course_app.app.tasks.tasks import send_booking_confirmation_email
 
 
 router = APIRouter(prefix='/bookings', tags=['Бронирования'])
@@ -25,10 +21,12 @@ async def get_bookings(user: Users = Depends(get_current_user)): # -> list[Booki
 
 @router.post('')
 async def add_booking(room_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
-    new_booking = await BookingsService.add(user.id, room_id, date_from, date_to)
-    if new_booking is None:
+    new_booking_id = await BookingsService.add(user.id, room_id, date_from, date_to)
+    if new_booking_id is None:
         raise RoomCannotBeBookedException
-    
+    new_booking = await BookingsService.find_by_id(booking_id=new_booking_id)
+    new_booking_dict = TypeAdapter(dict).validate_python(new_booking)
+    send_booking_confirmation_email.delay(booking=new_booking_dict, email_to=user.email)
 
 @router.delete('/{booking_id}', status_code=204)
 async def delete_booking(booking_id: int, user: Users = Depends(get_current_user)):
--- fastapi_course_app/app/bookings/service.py
+++ fastapi_course_app/app/bookings/service.py
@@ -1,9 +1,10 @@
 from datetime import date
-
 from sqlalchemy import and_, func, insert, or_, select
+
 from app.service.base import BaseService
 from app.bookings.models import Bookings
 from app.hotels.rooms.models import Rooms
+from app.hotels.models import Hotels
 from app.database import async_session_maker, engine
 
 
@@ -42,23 +43,48 @@ class BookingsService(BaseService):
                     date_from=date_from,
                     date_to=date_to,
                     price=price 
-                ).returning(Bookings)
+                ).returning(Bookings.id)
 
                 new_booking = await session.execute(add_booking)
                 await session.commit()
                 return new_booking.scalar()
             else:
                 return None
-
+            
+    @classmethod
+    def __get_bookings_join(cls, join_hotels=False):
+        if join_hotels:
+            return select(
+                Bookings.__table__.columns,
+                Rooms.image_id.label('room_image_id'), Rooms.name.label('room_name'),
+                Hotels.name.label('hotel_name'), Hotels.location.label('hotel_location')
+                ).select_from(Bookings).join(
+                    Rooms, Bookings.room_id == Rooms.id
+            ).join(
+            Hotels, Rooms.hotel_id == Hotels.id
+        )
+        return select(
+            Bookings.__table__.columns,
+            Rooms.image_id, Rooms.name, Rooms.description, Rooms.services
+            ).select_from(Bookings).join(
+                Rooms, Bookings.room_id == Rooms.id
+        )
+        
     @classmethod     
     async def find_all(cls, user_id: int):
         async with async_session_maker() as session:
-            bookings_query = select(
-                Bookings.__table__.columns,
-                Rooms.image_id, Rooms.name, Rooms.description, Rooms.services
-                ).select_from(Bookings).join(
-                Rooms, Bookings.room_id == Rooms.id
-            ).filter(Bookings.user_id == user_id)
+            bookings_query = cls.__get_bookings_join().where(
+                Bookings.user_id == user_id
+            )
             result = await session.execute(bookings_query)
             return result.mappings().all()
+        
+    @classmethod
+    async def find_by_id(cls, booking_id: int):
+        async with async_session_maker() as session:
+            bookings_query = cls.__get_bookings_join(join_hotels=True).where(
+                Bookings.id == booking_id
+            )
+            result = await session.execute(bookings_query)
+            return result.mappings().one_or_none()
         
